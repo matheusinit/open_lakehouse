55071fc86c67accd5c26035ec9ac0234aa293f5c
matheusinit <matheus.oliveira.s@protonmail.com>
2026-09-08 09:00:12 -0300

    refactor: ensure to remove scd2 suffix and remove information about the tool on model docs

--- tests/dim_customer_scd2_one_current_row.sql
+++ tests/dim_customer_scd2_one_current_row.sql
@@ -1,5 +1,5 @@
 select customer_id
-from {{ ref('dim_customer_scd2') }}
+from {{ ref('dim_customer') }}
 where is_current
 group by customer_id
 having count(*) > 1
--- tests/dim_customer_scd2_valid_range.sql
+++ tests/dim_customer_scd2_valid_range.sql
@@ -1,4 +1,4 @@
 select customer_key
-from {{ ref('dim_customer_scd2') }}
+from {{ ref('dim_customer') }}
 where valid_to is not null
   and valid_to <= valid_from
